9f53b6626bcb6e4e03d85119d8f54be2adab5729
John Quigley <jmquigs@gmail.com>
2015-12-16 20:17:38 -0500

    Rustfmt (minus some botched formats)

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,16 @@ extern crate rustc_serialize;
 use std::io;
 use std::io::Read;
 use std::sync::Arc;
-use std::sync::mpsc::{channel,Sender};
+use std::sync::mpsc::{channel, Sender};
 use std::thread;
 use std::time;
 use std::process::Command;
 use std::sync::Mutex;
 use std::net::SocketAddr;
 use std::net::TcpListener;
-//use std::cell::RefCell;
+// use std::cell::RefCell;
 
-use hyper::{Client,Server};
+use hyper::{Client, Server};
 use hyper::header::Connection;
 use hyper::server::Request;
 use hyper::server::Response;
@@ -26,32 +26,32 @@ use rustc_serialize::json;
 #[derive(Debug,Clone,RustcDecodable,RustcEncodable,PartialEq)]
 pub enum PWorkerState {
     JustLaunched,
-    Connected (i32),
-    Disconnected (u32), // time remaining in seconds before exit
+    Connected(i32),
+    Disconnected(u32), // time remaining in seconds before exit
 }
 
 #[derive(Debug,Clone,RustcDecodable,RustcEncodable,PartialEq)]
 pub enum FollowerState {
     NotSpawned,
-    Spawned(u32) // pid of spawned process
+    Spawned(u32), // pid of spawned process
 }
 
 #[derive(Debug,Clone,RustcDecodable,RustcEncodable,PartialEq)]
 pub enum PWorkerResponse {
     NoResponseYet,
-    WorkerAlive(PWorkerState,FollowerState),
+    WorkerAlive(PWorkerState, FollowerState),
     AttachedToExistingWorker,
-    GenericError(String)
+    GenericError(String),
 }
 
-pub fn spawn_follower(command:&mut Command) -> io::Result<u32> {
+pub fn spawn_follower(command: &mut Command) -> io::Result<u32> {
     println!("spawning follower");
     let child = command.spawn();
     let child = try!(child);
     Ok(child.id())
 }
 
-pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerResponse>) {
+pub fn start_or_attach(command: &mut Command, port: u16, resp_tx: Sender<PWorkerResponse>) {
     let resp_tx = resp_tx.clone();
 
     // connect to existing pworker, pass my pid
@@ -83,22 +83,24 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
     let set_new_parent =
         // only do this if we know port is bound, to avoid possible connection hang
         if port_bound {
-            let pweb = format!("http://127.0.0.1:{}/?parent_pid={}", port, ppid);
-            println!("pworker url: {}", pweb);
+        let pweb = format!("http://127.0.0.1:{}/?parent_pid={}", port, ppid);
+        println!("pworker url: {}", pweb);
 
-            match client.get(&pweb)
-                .header(Connection::close())
-                .send() {
-                    Err(e) => {
-                        // the port is bound but we couldn't set a new parent. error
-                        let _ = resp_tx.send(PWorkerResponse::GenericError(format!("Port bound but unable to connect to existing pworker: {}", e)));
-                        return;
-                    },
-                    Ok(_) => true
-                }
-        } else {
-            false
-        };
+        match client.get(&pweb)
+                    .header(Connection::close())
+                    .send() {
+            Err(e) => {
+                // the port is bound but we couldn't set a new parent. error
+                let msg = format!("Port bound but unable to connect to existing pworker: {}",
+                                  e);
+                let _ = resp_tx.send(PWorkerResponse::GenericError(msg));
+                return;
+            }
+            Ok(_) => true,
+        }
+    } else {
+        false
+    };
     println!("set parent: {}", set_new_parent);
 
     // spawn thread to monitor pworker http server and replicate messages on
@@ -115,17 +117,20 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
             loop {
                 let pweb = format!("http://127.0.0.1:{}/?sitrep", port);
                 let res = client.get(&pweb)
-                    .header(Connection::close())
-                    .send();
+                                .header(Connection::close())
+                                .send();
                 match res {
                     Err(e) => {
                         fail_count += 1;
                         println!("fail: new count: {}; {}", fail_count, e);
                         if fail_count > max_fail {
-                            let _ = resp_tx.send(PWorkerResponse::GenericError(format!("Unable to connect to pworker after {} attempts: {}", fail_count, e)));
+                            let msg = format!("Unable to connect to pworker after {} attempts: {}",
+                                              fail_count,
+                                              e);
+                            let _ = resp_tx.send(PWorkerResponse::GenericError(msg));
                             break;
                         }
-                    },
+                    }
                     Ok(res) => {
                         fail_count = 0;
                         let mut body = String::new();
@@ -134,7 +139,7 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
                         let new_state: PWorkerResponse = json::decode(&body).unwrap();
 
                         if new_state != last_resp {
-                            //println!("hresp, newstate: {:?}", new_state);
+                            // println!("hresp, newstate: {:?}", new_state);
                             last_resp = new_state;
                             let _ = resp_tx.send(last_resp.clone());
                         }
@@ -186,15 +191,15 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
                         RequestUri::AbsolutePath(p) => {
                             if p.contains("parent_pid=") {
                                 let res = p.split("parent_pid=");
-                                let vec:Vec<&str> = res.collect();
+                                let vec: Vec<&str> = res.collect();
                                 if vec.len() == 2 {
                                     let new_pid = vec[1].parse::<i32>().unwrap();
                                     let l_tx = h_tx.lock().unwrap();
                                     l_tx.send(new_pid).unwrap();
                                 }
                             }
-                        },
-                        x => panic!("bizzaro request: {}", x)
+                        }
+                        x => panic!("bizzaro request: {}", x),
                     }
 
                     {
@@ -209,29 +214,30 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
                     let state = h_state.lock().ok().expect("failed to lock state in handler");
                     let fstate = h_fstate.lock().ok().expect("failed to lock fstate in handler");
 
-                    let ws = PWorkerResponse::WorkerAlive((*(*state)).clone() ,(*(*fstate)).clone());
+                    let ws = PWorkerResponse::WorkerAlive((*(*state)).clone(),
+                                                          (*(*fstate)).clone());
 
                     let encoded = json::encode(&ws).unwrap();
 
-                    //println!("handler ws: {:?}", ws);
+                    // println!("handler ws: {:?}", ws);
 
                     let _ = res.send(encoded.as_bytes());
                 };
 
                 // make http server
-                let mut server =  {
+                let mut server = {
                     let s_str = format!("127.0.0.1:{}", port);
                     let addr: SocketAddr = s_str.parse().unwrap();
                     let server = Server::http(addr).unwrap();
                     println!("starting pworker server");
                     match server.handle_threads(hb_handler, 2) {
                         Err(e) => panic!("failed to start http server: {}", e),
-                        Ok(l) => l
+                        Ok(l) => l,
                     }
                 };
 
                 // setup termination handler
-                let mut terminate = |fstate:&FollowerState| {
+                let mut terminate = |fstate: &FollowerState| {
                     unsafe {
                         if let FollowerState::Spawned(pid) = *fstate {
                             println!("killing follower");
@@ -251,33 +257,42 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
                     while let Ok(new_pid) = rx.try_recv() {
                         println!("got new pid: {}", new_pid);
                         ppid = new_pid;
-                    };
+                    }
 
                     // kill with signal 0 to check to see if ppid is alive
                     let par_alive = unsafe { libc::kill(ppid, 0) } == 0;
-                    //println!("pworker state: {:?}; par alive: {}", state, par_alive);
+                    // println!("pworker state: {:?}; par alive: {}", state, par_alive);
 
                     // clone the states to make the check logic easier to deal with
-                    let old_fstate = *(fstate.lock().ok().expect("Failed to lock fstate prior to state update")).clone();
-                    let old_state = *(state.lock().ok().expect("Failed to lock state prior to state update")).clone();
+                    let old_fstate = *(fstate.lock()
+                                             .ok()
+                                             .expect("Failed to lock fstate prior to state \
+                                                      update"))
+                                          .clone();
+                    let old_state = *(state.lock()
+                                           .ok()
+                                           .expect("Failed to lock state prior to state update"))
+                                         .clone();
 
                     let new_state = {
                         if par_alive {
                             match old_state {
                                 // always reset to connected if parent alive
-                                PWorkerState::Connected(_)
-                                | PWorkerState::Disconnected(_)
-                                | PWorkerState::JustLaunched => PWorkerState::Connected(ppid)
+                                PWorkerState::Connected(_) |
+                                PWorkerState::Disconnected(_) |
+                                PWorkerState::JustLaunched => PWorkerState::Connected(ppid),
                             }
                         } else {
                             // parent disconnected
                             match old_state {
-                                PWorkerState::JustLaunched
-                                | PWorkerState::Connected(_) => PWorkerState::Disconnected(5),
-                                PWorkerState::Disconnected(n) if n > 0 => PWorkerState::Disconnected(n-1),
+                                PWorkerState::JustLaunched |
+                                PWorkerState::Connected(_) => PWorkerState::Disconnected(5),
+                                PWorkerState::Disconnected(n) if n > 0 => {
+                                    PWorkerState::Disconnected(n - 1)
+                                }
                                 PWorkerState::Disconnected(0) => terminate(&old_fstate),
                                 // this is an illegal state, reset to something legal
-                                PWorkerState::Disconnected(_) => PWorkerState::Disconnected(0)
+                                PWorkerState::Disconnected(_) => PWorkerState::Disconnected(0),
                             }
                         }
                     };
@@ -287,7 +302,12 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
                         if let FollowerState::NotSpawned = old_fstate {
                             match spawn_follower(command) {
                                 Err(e) => println!("Unable to spawn follower: {}", e),
-                                Ok(fpid) => *(*(fstate.lock().ok().expect("failed to lock fstate for update"))) = FollowerState::Spawned(fpid)
+                                Ok(fpid) => {
+                                    *(*(fstate.lock()
+                                              .ok()
+                                              .expect("failed to lock fstate for update"))) =
+                                        FollowerState::Spawned(fpid)
+                                }
                             }
                         }
                     }
@@ -296,12 +316,11 @@ pub fn start_or_attach(command:&mut Command, port:u16, resp_tx:Sender<PWorkerRes
 
                     thread::sleep(time::Duration::from_millis(1000));
                 }
-            }
-            else {
+            } else {
                 // exit on error, or remnant from double-fork
-                unsafe {libc::exit(0)};
+                unsafe { libc::exit(0) };
             }
-        },
+        }
         n => {
             println!("pworker spawned: {}", n);
         }
@@ -315,7 +334,7 @@ mod tests {
     use std::env;
     use std::time;
     use std::thread;
-    use std::sync::mpsc::{channel};
+    use std::sync::mpsc::channel;
 
     extern crate libc;
 
@@ -352,12 +371,14 @@ mod tests {
         let mut got_alive = false;
         for _ in 1..scount {
             while let Ok(m) = rx.try_recv() {
-                if let PWorkerResponse::WorkerAlive(PWorkerState::Connected(conpid), FollowerState::Spawned(fpid)) = m {
+                if let PWorkerResponse::WorkerAlive(PWorkerState::Connected(conpid),
+                                                    FollowerState::Spawned(fpid)) = m {
                     assert!(conpid == my_pid, "expected my pid but got a different one");
 
                     // kill the foller with sig 0 to make sure it is really alive
-                    let f_alive = unsafe { libc::kill(fpid as i32,0) } == 0;
-                    assert!(f_alive, format!("expected follower pid {} to be alive, but it isn't", fpid));
+                    let f_alive = unsafe { libc::kill(fpid as i32, 0) } == 0;
+                    assert!(f_alive,
+                            format!("expected follower pid {} to be alive, but it isn't", fpid));
 
                     got_alive = true;
                 }
